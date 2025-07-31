88b0adb330727c14456892e705d2661cd6b287dd
Levis Ngakop <levisngakop47@gmail.com>
2025-07-31 06:30:37 +0100

    new tittle for testimonial

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = ['home', 'about', 'projects', 'skills', 'certifications', 'blog', 'contact'];
+      const sections = ['home', 'about', 'projects', 'skills', 'certifications', 'blog', 'testimonials', 'contact'];
       const scrollPosition = window.scrollY + 100;
 
       for (const section of sections) {
